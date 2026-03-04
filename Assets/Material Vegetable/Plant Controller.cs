8302207bbae252dfa98e43a624e8999223af9b91
soravitkung <soravit.saeh@bumail.net>
2026-03-04 13:22:38 +0700

    Plant UI button Test

---
+++ Assets/Material Vegetable/Plant Controller.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlantController : MonoBehaviour
+{
+    [Header("Settings")]
+    public float timePerStage = 5f; // time duration
+
+    [Header("Visual Models")]
+    public GameObject seedModel;     // fade 1
+    public GameObject seedlingModel; // fade 2
+    public GameObject matureModel;   // fade 3
+
+    private int currentStage = 1;
+    private float timer = 0;
+    private bool isFullyGrown = false;
+
+    void Start()
+    {
+        UpdateVisuals(); // show seed
+    }
+
+    void Update()
+    {
+        if (isFullyGrown) return;
+
+        timer += Time.deltaTime;
+        if (timer >= timePerStage)
+        {
+            Grow();
+        }
+    }
+
+    void Grow()
+    {
+        currentStage++;
+        timer = 0;
+        UpdateVisuals();
+
+        if (currentStage >= 3) isFullyGrown = true;
+    }
+
+    void UpdateVisuals()
+    {
+        if (seedModel) seedModel.SetActive(currentStage == 1);
+        if (seedlingModel) seedlingModel.SetActive(currentStage == 2);
+        if (matureModel) matureModel.SetActive(currentStage == 3);
+    }
+}
---
+++ Assets/Material Vegetable/PlantSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlantSpawnerUI : MonoBehaviour
+{
+    public GameObject plantPrefab; 
+
+    public void SpawnPlant()
+    {
+        
+        Vector3 randomPos = new Vector3(Random.Range(-5f, 5f), 0, Random.Range(-5f, 5f));
+
+        
+        Instantiate(plantPrefab, randomPos, Quaternion.identity);
+    }
+}
